dfb042f0857025b26332e6ad6c9990f7290af2f8
LarryLai1 <noiselarry1234@gmail.com>
2024-06-14 19:20:50 +0800

    finish main program

--- generate.py
+++ generate.py
@@ -3,19 +3,23 @@ import copy
 
 app = 0
 rmv = 0
+count = 0
 
-def dfs(ind: int, sep: int, temp: list, single_team: set, single_team_unused: list, level_used: np.array, team_used: np.array)->bool:
+def dfs(ind: int, sep: int, temp: list, level_used: np.array, team_used: np.array, shift: int)->bool:
+    global count
     jump = 1
+    threshold = 0 if sep==0 or ind<team_amount*time_amount/4 else 1
+
+    # stop condition
     if ind == team_amount*time_amount/2:
+        if shift!=count:
+            count+=1
+            return False
         ans.append(temp)
         total_ans[sep] = copy.deepcopy(ans)
-        print("add")
         return True
-    # print(f'ind: {ind}')
+    
     time, index = int(ind/(team_amount/2)), ind%(team_amount/2)
-    tt = total_ans[sep][time]
-    # if ind==18:
-    #     print(f"temp: {temp}")
 
     # change level
     if index == 0:
@@ -27,56 +31,22 @@ def dfs(ind: int, sep: int, temp: list, single_team: set, single_team_unused: li
         temp = [() for _ in range(level_amount)]
         level_used = np.zeros(level_amount, dtype=int)
         team_used = np.zeros(team_amount+1, dtype=int)
-        single_team = set()
-        single_team_unused = []
-        for i in range(0, level_amount):
-            if tt[i][0]:
-                team_used[tt[i][0]] = 1
-                if tt[i][1]:
-                    level_used[i] = 1
-                    team_used[tt[i][1]] = 1
-                    temp[i] = tt[i]
-                    jump += 1
-                else:
-                    single_team_unused.append(i)
-                    single_team.add(i)
-    # print(f"single_team: {single_team}")
-    if jump>1:
-        print(f"jump a lot at {time}")
-    # single team
-    for id, tm in enumerate(single_team_unused):
-        t1 = tt[tm][0]
-        for t2 in range(3, team_amount+1):
-            if team_used[t2] or tm not in level_table[t2]:
-                continue
-            team_used[t2] = 1
-            level_used[tm] = 1
-            level_table[t2].remove(tm)
-            temp[tm] = (t1, t2)
-            single_team_unused.pop(id)
-            if dfs(ind+jump, sep, temp, single_team, single_team_unused, level_used, team_used):
-                return True
-            team_used[t2] = 0
-            level_used[tm] = 0
-            level_table[t2].add(tm)
-            temp[tm] = ()
-            single_team_unused.insert(id, tm)
     
-    # dual team
-    for t1 in range(3, team_amount+1):
+    # matching
+    for t1 in range(1, team_amount+1):
         if team_used[t1]:
             continue
         team_used[t1] = 1
         for t2 in range(t1+1, team_amount+1):
             if team_used[t2]:
                 continue
-            if meet_table[t1][t2]:
+            if meet_table[t1][t2]>threshold:
                 continue
             team_used[t2] = 1
-            meet_table[t1][t2] = 1
-            meet_table[t2][t1] = 1
+            meet_table[t1][t2] += 1
+            meet_table[t2][t1] += 1
 
-            level_choice = level_table[t1].intersection(level_table[t2]) - single_team
+            level_choice = level_table[t1].intersection(level_table[t2])
             if len(level_choice) == 0:
                 continue
             # print(level_choice)
@@ -85,29 +55,29 @@ def dfs(ind: int, sep: int, temp: list, single_team: set, single_team_unused: li
                     continue
                 level_table[t1].remove(level)
                 level_table[t2].remove(level)
-                level_used[level] = 1
+                level_used[level] += 1
                 temp[level] = (t1, t2)
-                if dfs(ind+jump, sep, temp, single_team, single_team_unused, level_used, team_used):
+                if dfs(ind+jump, sep, temp, level_used, team_used, shift):
                     return True
                 level_used[level] = 0
                 level_table[t1].add(level)
                 level_table[t2].add(level)
                 temp[level] = ()
             team_used[t2] = 0
-            meet_table[t1][t2] = 0
-            meet_table[t2][t1] = 0
+            meet_table[t1][t2] -= 1
+            meet_table[t2][t1] -= 1
         team_used[t1] = 0
     if index==0 and time!=0:
-        global rmv
-        ans.remove(last_temp)
-        rmv+=1
-    # print("rollback")
+        if last_temp in ans:
+            global rmv
+            ans.remove(last_temp)
+            rmv+=1
     return False
 
 level_amount = 5
-time_amount = 3
+time_amount = 4
 team_amount = 10
-seperation = 1
+seperation = 2
 meet_table = np.zeros((team_amount+1, team_amount+1), dtype=int)
 
 total_ans = [[[(None, None) for x in range(level_amount)] for a in range(time_amount)] for _ in range(seperation)]
@@ -115,43 +85,25 @@ total_ans = [[[(None, None) for x in range(level_amount)] for a in range(time_am
 if seperation*time_amount > team_amount-1:
     raise ValueError("Impossible Input Data")
 
-
-def pre_fill():
-    meet_table = np.zeros((team_amount+1, team_amount+1), dtype=int)
-    total_ans = [[[(None, None) for x in range(level_amount)] for a in range(time_amount)] for _ in range(seperation)]
-    # pre-set 1
-    for i in range(seperation):
-        for j in range(time_amount):
-            total_ans[i][j][j] = (1, None)
-
-    # pre-set 2
-    total_ans[0][0][0] = (1, 2)
-    for i in range(seperation):
-        for j in range(time_amount):
-            if (i==j and j==0): continue
-            else:
-                if i==0:
-                    index = j-1+2*(j%2)
-                else:
-                    index = (j+1)%level_amount
-                total_ans[i][j][index] = (2, None)
-
-    # fix more
-    meet_table[1][2] = 1
-    meet_table[2][1] = 1
-    return meet_table, total_ans
-
-
-# meet_table, total_ans = pre_fill()
 for sep in range(seperation):
     level_table = [set([i for i in range(0, level_amount)]) for _ in range(team_amount+1)]
     ans = []
-    print("---"*10)
-    if sep:
-        print('meet_table')
-        print(meet_table)
-    dfs(0, sep, [], [], [], np.zeros(level_amount, dtype=int), np.zeros(team_amount+1, dtype=int))
-    print('total_ans')
-    for e in total_ans[sep]:
+    count = 0
+    dfs(0, sep, [], np.zeros(level_amount, dtype=int), np.zeros(team_amount+1, dtype=int), 3)
+
+print("Time Table for Levels: ")
+for d in total_ans:
+    for e in d:
         print(e)
-    print(app, rmv)
+
+print("--"*20)
+print("Time Table for Teams: ")
+for tm in range(1, team_amount+1):
+    print(f"Team {tm:2}: ", end=" ")
+    for sep in range(seperation):
+        for time in range(0, time_amount):
+            for level in range(0, level_amount):
+                if tm in total_ans[sep][time][level]:
+                    print(level, end="->")
+                    break
+    print("ED")
